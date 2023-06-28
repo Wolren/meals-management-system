59af1991e947d02149ca2b73c83029d6d6d690e2
Mateusz Dobrzynski <mateusz.dobrzynski64@gmail.com>
2023-06-28 19:55:12 +0200

    [7] create an empty Springboot app

---
+++ src/test/java/org/lookout_studios/meals_management_system/meals_management_system/MealsManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lookout_studios.meals_management_system.meals_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealsManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
