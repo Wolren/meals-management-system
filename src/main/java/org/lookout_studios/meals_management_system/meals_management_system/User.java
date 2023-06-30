7b4789e821316454f3e813016ad32f7172f312d7
Mateusz Dobrzynski <mateusz.dobrzynski64@gmail.com>
2023-06-30 15:39:55 +0200

    [19] create User class and add basic request handling

---
+++ src/main/java/org/lookout_studios/meals_management_system/meals_management_system/User.java
@@ -0,0 +1,23 @@
+package org.lookout_studios.meals_management_system.meals_management_system;
+
+public class User {
+    private int userId;
+    private String email;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
