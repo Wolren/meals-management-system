026f2c275500e46f367b30b42ee0737cb7131a08
Mateusz Dobrzynski <mateusz.dobrzynski64@gmail.com>
2023-08-10 19:32:23 +0200

    [19] introduce RegistrationService integration tests

--- src/test/java/org/lookout_studios/meals_management_system/meals_management_system/EstablishConnectionWithDatabaseTest.java
+++ src/test/java/org/lookout_studios/meals_management_system/meals_management_system/EstablishConnectionWithDatabaseTest.java
@@ -9,26 +9,28 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
- 
+
 public class EstablishConnectionWithDatabaseTest {
- 
-  @InjectMocks private EstablishConnectionWithDatabase dbConnectionMock;
-  @Mock private Connection mockConnection;
-  @Mock private Statement mockStatement;
- 
+
+  @InjectMocks
+  private EstablishConnectionWithDatabase dbConnectionMock;
+  @Mock
+  private Connection mockConnection;
+  @Mock
+  private Statement mockStatement;
+
   @Before
   public void setUp() {
     MockitoAnnotations.openMocks(this);
   }
- 
+
   @Test
   public void testMockDBConnection() {
     try {
       Mockito.when(mockConnection.createStatement()).thenReturn(mockStatement);
       Mockito.when(mockConnection.createStatement().executeUpdate(Mockito.any())).thenReturn(1);
       Mockito.verify(mockConnection.createStatement(), Mockito.times(1));
-    }
-    catch (Exception mysqlError) {
+    } catch (Exception mysqlError) {
       System.out.println(mysqlError);
     }
   }
--- src/test/java/org/lookout_studios/meals_management_system/meals_management_system/RegistrationServiceTests.java
+++ src/test/java/org/lookout_studios/meals_management_system/meals_management_system/RegistrationServiceTests.java
@@ -2,12 +2,29 @@ package org.lookout_studios.meals_management_system.meals_management_system;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class RegistrationServiceTests {
+    @Mock
+    RegistrationService registrationService = new RegistrationService();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    /**
+     * Ensures that emailCheck returns true with valid emails
+     */
     @Test
     void validEmailCheck() {
-        RegistrationService registrationService = new RegistrationService();
         assertTrue(registrationService.emailCheck("example@gmail.com"));
         assertTrue(registrationService.emailCheck("example1234@gmail.com"));
         assertTrue(registrationService.emailCheck("abc-d@mail.com"));
@@ -17,13 +34,75 @@ class RegistrationServiceTests {
         assertTrue(registrationService.emailCheck("abc123@mail.com"));
     }
 
+    /**
+     * Ensures that emailCheck returns false with invalid emails
+     */
     @Test
     void invalidEmailCheck() {
-        RegistrationService registrationService = new RegistrationService();
         assertFalse(registrationService.emailCheck("abc-@mail.com"));
         assertFalse(registrationService.emailCheck("abc..def@mail.com"));
         assertFalse(registrationService.emailCheck("abc#def@mail.com"));
         assertFalse(registrationService.emailCheck("abc()@mail.com"));
         assertFalse(registrationService.emailCheck("abc^&$@mail.com"));
+    }
+
+    /**
+     * Ensures that /register requests with invalid emails generate
+     * InvalidEmailException
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void registerInvalidEmailUserFail() throws Exception {
+        User invalidEmailUser = new User("abc-@mail.com", null);
+        Mockito
+                .when(registrationService.registerUser(invalidEmailUser))
+                .thenThrow(new InvalidEmailException());
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(invalidEmailUser.toString());
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isBadRequest());
+    }
+
+    /**
+     * Ensures that /register requests with invalid passwords generate
+     * InvalidPasswordException
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void registerInvalidPasswordUserFail() throws Exception {
+        User invalidEmailUser = new User("abc@mail.com", null);
+        Mockito
+                .when(registrationService.registerUser(invalidEmailUser))
+                .thenThrow(new InvalidPasswordException());
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(invalidEmailUser.toString());
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isBadRequest());
+    }
+
+    /**
+     * Ensures that /register requests with emails of existing users generate
+     * ExistingUserException
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void registerExistingUserFail() throws Exception {
+        User invalidEmailUser = new User("abc@mail.com", null);
+        Mockito
+                .when(registrationService.registerUser(invalidEmailUser))
+                .thenThrow(new RegisteredUserException());
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(invalidEmailUser.toString());
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isBadRequest());
     }
 }
