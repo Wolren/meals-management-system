3bbdc81e5fc3e863a0b75f18e09a523de21f1158
Mateusz Dobrzynski <mateusz.dobrzynski64@gmail.com>
2023-08-10 17:13:07 +0200

    [19] introduce RegisteredUserException

--- src/main/java/org/lookout_studios/meals_management_system/meals_management_system/RegistrationService.java
+++ src/main/java/org/lookout_studios/meals_management_system/meals_management_system/RegistrationService.java
@@ -10,13 +10,18 @@ import java.util.regex.Pattern;
 @RestController
 public class RegistrationService {
 
-    /*
+    /**
      * This method handles registration requests and sends confirmation emails.
+     * 
+     * @throws RegisteredUserException
      */
     @PostMapping(value = "/register", consumes = "application/json", produces = "application/json")
-    public ResponseEntity registerUser(@RequestBody User user) throws InvalidEmailException {
+    public ResponseEntity registerUser(@RequestBody User user) throws InvalidEmailException, RegisteredUserException {
         if (!emailCheck(user.getEmail())) {
             throw new InvalidEmailException();
+        }
+        if (!newUserCheck(user.getEmail())) {
+            throw new RegisteredUserException();
         }
         String registrationToken = user.generateRegistrationToken();
         user.register();
@@ -24,7 +29,7 @@ public class RegistrationService {
         return ResponseEntity.ok().build();
     }
 
-    /*
+    /**
      * This method checks whether an email address has a correct format
      */
     public boolean emailCheck(String email) {
@@ -34,4 +39,16 @@ public class RegistrationService {
         return answer;
     }
 
+    /**
+     * Checks whether the provided email address is present in the database.
+     * 
+     * @param email Email address of the user.
+     * @return True if the email address is new, false if it belongs to an existing
+     *         user.
+     */
+    public boolean newUserCheck(String email) {
+        // TO-DO: Execute a query to check whether the email is in the database.
+        // If so, return false
+        return true;
+    }
 }
