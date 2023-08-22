2603aba7e6aa1e2d60ff50ed383ae1380859ba7b
Hubert Borysowski <borysowski.git@gmail.com>
2023-08-22 14:33:39 +0200

    [18] Swagger implementation through OpenAPI 📄Springfox swagger is no longer supported by Springboot 3 as before. Being so, I changed swagger dependencies to OpenAPI dependency, which has swagger-ui built in.

--- src/main/java/org/lookout_studios/meals_management_system/meals_management_system/MealsManagementSystemApplication.java
+++ src/main/java/org/lookout_studios/meals_management_system/meals_management_system/MealsManagementSystemApplication.java
@@ -4,16 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-
 /*
  * This class handles the app logic.
  * TO-DO: Remove (exclude = { DataSourceAutoConfiguration.class })
  * once the database support is implemented.
  */
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-@EnableSwagger2
 public class MealsManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MealsManagementSystemApplication.class, args);
--- src/test/java/org/lookout_studios/meals_management_system/meals_management_system/DatabaseServiceTest.java
+++ src/test/java/org/lookout_studios/meals_management_system/meals_management_system/DatabaseServiceTest.java
@@ -15,7 +15,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
-public class EstablishConnectionWithDatabaseTest {
+public class DatabaseServiceTest {
 
     @InjectMocks
     private DatabaseService dbConnectionMock;
